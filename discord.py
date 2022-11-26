d54eb19a09f24510fef5171fe8ebf805f53c5f48
Karnikaa Velumani <karnikaavelumani@csu.fullerton.edu>
2022-11-25 20:45:38 -0800

    Set discord bot status online

---
+++ discord.py
@@ -0,0 +1,13 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+class CustomClient(discord.Client):
+    async def on_ready(self):
+        print(f'{self.user} has connected to Discord!')
+
+client = CustomClient()
+client.run(TOKEN)
