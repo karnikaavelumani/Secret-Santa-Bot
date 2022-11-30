da1393f7be7cbdc53d237368bdf34bd88d0102c5
Karnikaa Velumani <karnikaavelumani@csu.fullerton.edu>
2022-11-30 13:26:28 -0800

    Create simple slash command

--- santa_bot.py
+++ santa_bot.py
@@ -1,25 +1,28 @@
 import discord
 import os
 from dotenv import load_dotenv
+from discord.ext import commands
 
+# Load discord token from .env file
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-intents = discord.Intents.default()
+# Set all permissions for discord bot
+intents = discord.Intents.all()
 intents.message_content = True
-
 client = discord.Client(intents=intents)
 
+# Set bot prefix as slash
+client = commands.Bot(command_prefix='/', intents=intents)
+
+# Set bot status online
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('world!')
+# Basic slash command ( /test )
+@client.command()
+async def test(ctx):
+    await ctx.send("test")
 
 client.run(TOKEN)
